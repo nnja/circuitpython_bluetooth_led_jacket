36ec9086859544483259e47dea0fb53318740910
Nina Zakharenko <nzakharenko@gmail.com>
2020-06-13 17:39:33 -0700

    Update CircuitPython version to 5.3.0

--- README.md
+++ README.md
@@ -2,20 +2,25 @@
 
 This code is for an LED jacket with 3D printed embellishments, built by following a guide by Sophy Wong in her book [Wearable Tech projects](https://www.raspberrypi.org/blog/create-wearable-tech-projects-with-sophy-wong/).
 
-The code is run on an [Adafruit Feather nRF52840 Express](https://www.adafruit.com/product/4062) with Bluetooth.
+The code targets an [Adafruit Feather nRF52840 Express](https://www.adafruit.com/product/4062) with Bluetooth running CircuitPython 5.3.0.
 
 The color of the default pattern can be changed by connecting to the board with a phone running [Adafruit's Bluefruit LE Connect App](https://learn.adafruit.com/bluefruit-le-connect) for iOS or Android. Sending a color will change the hue of the running pattern. 
 
 The generative pattern was written in collaboration with [@jacobjoaquin](https://github.com/jacobjoaquin). 
 
-## Photo
+## Preview
 
-![](https://user-images.githubusercontent.com/2030983/84581729-4a843000-ad99-11ea-8282-ba67f90bb705.jpg)
+![](https://user-images.githubusercontent.com/2030983/84583316-22ea9300-adac-11ea-9b62-adcae3b21277.gif)
+
 
 ## Future Plans
 
-Currently the LED pattern runs quite slowly, likely because the code is always listening for Bluetooth packets. 
+It would be nice to include an OLED feather wing display to display text status updates and add further interactivity using the onboard buttons.
 
-I'd like to try using the new [CircuitPython LED Animation library](https://github.com/adafruit/Adafruit_CircuitPython_LED_Animation), or potentially a cooperative multitasking approach using generators as outlined by Adam Forsyth in his [June 2020 Chicago Python talk](https://www.youtube.com/watch?v=g6dGfJ8oCqg).
+Other Ideas:
 
-Additionally, it would be nice to include an OLED feather wing display to display text status updates and add further interactivity using the onboard buttons.
+- use up/down to cycle between brightness
+- use left/right to cycle between patterns
+- think about how to "play" multiple patterns
+- find code with various patterns in backup
+- try using the [adafruit_led_animation library](https://github.com/adafruit/Adafruit_CircuitPython_LED_Animation) to switch between patterns
--- code.py
+++ code.py
@@ -1,11 +1,11 @@
 """
-Ideas:
-- use up/down to cycle between brightness
-- use left/right to cycle between patterns
-- think about how to "play" multiple patterns
-    maybe use adam's code with generators as inspiration
-    check out examples from Adam https://github.com/agfor/PyCon2019/blob/master/Attendee_Examples/music_and_lights_using_generators.py
-- find code with various patterns in backup
+Copyright 2020 Nina Zakharenko
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import board
@@ -24,14 +24,11 @@ ble = BLERadio()
 uart_service = UARTService()
 advertisement = ProvideServicesAdvertisement(uart_service)
 
-
-
 num_pixels = 5
 
 pixels = neopixel.NeoPixel(
     board.D12,
     num_pixels,
-    # brightness=1,
 )
 onboard_pixel = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.01)
 
@@ -40,10 +37,7 @@ print("Starting...")
 def chance(odds):
     return random.random() < odds
 
-# odds_to_trigger = 0.25
 odds_to_trigger = 0.3
-
-
 
 color_list = [(0, 0, 255), (0, 255, 255), (255, 255, 0), (255, 0, 255)]
 led_color_list = []
@@ -52,7 +46,7 @@ brightness_list = []
 brightness_list = [0.0] * num_pixels
 led_color_list = [0] * num_pixels
 
-def pattern(loop_sleep=0.2):
+def pattern():
     # Display random pattern with jewel tones
     for pixel in range(num_pixels):
         is_triggered = chance(odds_to_trigger)
@@ -68,47 +62,35 @@ def pattern(loop_sleep=0.2):
             b = int(b * this_color[2])
 
             pixels[pixel] = (r, g, b)
-            # print("setting pixel %s" % pixel)
-
-        # time.sleep(random.random() * 0.1 + 0.01)
         time.sleep(random.random() * 0.065 + 0.01)
 
-    # time.sleep(0.02)
-    time.sleep(loop_sleep)
+    time.sleep(random.random() * 0.02 + 0.01)
 
 while True:
-    print("at ntop of loop")
-
     # Advertise when not connected.
     ble.start_advertising(advertisement)
 
     while not ble.connected:
         pattern()
         onboard_pixel.fill((128, 0, 0))
-        # for val in (0, 64, 0, 128, 0, 192, 0, 255):
-            # onboard_pixel.fill((val, 0, 0))
-            # time.sleep(0.5)
+
+    ble.stop_advertising()
+
     while ble.connected:
-        # onboard_pixel.fill((0, 255, 0))
-        pattern(loop_sleep=0)
-        # print("BLE connected")
-        packet = Packet.from_stream(uart_service)
-        if isinstance(packet, ColorPacket):
-            print("Color Picked", packet.color)
-            # onboard_pixel.fill(packet.color)
-            pixels.fill((0, 0, 0))
-            color_list = [packet.color] * 4
-            onboard_pixel.fill(packet.color)
+        pattern()
+        if uart_service.in_waiting:
+            packet = Packet.from_stream(uart_service)
 
-        if isinstance(packet, ButtonPacket):
-            if packet.pressed:
-                if packet.button == ButtonPacket.BUTTON_1:
-                    # The 1 button was pressed.
-                    print("1 button pressed!")
-                    # reset colors
-                    color_list = [(0, 0, 255), (0, 255, 255), (255, 255, 0), (255, 0, 255)]
-                elif packet.button == ButtonPacket.UP:
-                    # The UP button was pressed.
-                    print("UP button pressed!")
+            if isinstance(packet, ColorPacket):
+                print("Color Picked", packet.color)
+                pixels.fill((0, 0, 0))
+                color_list = [packet.color] * 4
+                onboard_pixel.fill(packet.color)
 
-
+            if isinstance(packet, ButtonPacket):
+                if packet.pressed:
+                    if packet.button == ButtonPacket.BUTTON_1:
+                        # The 1 button was pressed.
+                        print("1 button pressed!")
+                        # reset colors
+                        color_list = [(0, 0, 255), (0, 255, 255), (255, 255, 0), (255, 0, 255)]
